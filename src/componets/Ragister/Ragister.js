116a15579e173d9e958c7346d155afc71ae2ee36
neaz2006 <45340875+neaz2006@users.noreply.github.com>
2020-11-16 03:05:20 +0300

    login page done

--- src/componets/Ragister/Ragister.js
+++ src/componets/Ragister/Ragister.js
@@ -6,29 +6,29 @@ import fbook from '../../images/fb.png'
 const Ragister = () => {
     return (
         <>
-        <div className="log">
+            <div className="log">
                 <Link to="/" className='text-white logo'><img className="logo" alt="" /></Link>
                 <div className="login">
                     <h2>Create an account </h2> <br />
                     <form action="">
-                        <input type="text" placeholder="Frist Name"className="input" /><br/>
-                        <input type="text" placeholder="Last Name"className="input" /><br/>
+                        <input type="text" placeholder="Frist Name" className="input" /><br />
+                        <input type="text" placeholder="Last Name" className="input" /><br />
                         <input type="email" name="" id="" placeholder="Email" className="input" /><br />
-                        <input type="password" name="" id="" placeholder="Password" className="input" /><br/>
-                        <input type="password" name="" id="" placeholder="Confrim Password" className="input" /><br/>
-                        <div className="text-center m-auto">   
-                            <button type="submit" className="btn">Create an account</button><br/><br/>
+                        <input type="password" name="" id="" placeholder="Password" className="input" /><br />
+                        <input type="password" name="" id="" placeholder="Confrim Password" className="input" /><br />
+                        <div className="text-center m-auto">
+                            <button type="submit" className="btn">Create an account</button><br /><br />
                             <p className="mt-5 m-auto">Already have an account?<Link rel="stylesheet" to="login">Login</Link></p>
                         </div>
                     </form>
                 </div>
-            </div><br/>
+            </div><br />
             <div className="text-center sign__with">
-               --------OR-------- <br/> <br/>
+                --------OR-------- <br /> <br />
                 <img className="google" src={fbook} alt="" />
-                <button className="btn_g ">Continue with Facebook</button><br/><br/>
+                <button className="btn_g ">Continue with Facebook</button><br /><br />
                 <img className="google" src={google} alt="" />
-                <button className="btn_g ">Continue with google</button>        
+                <button className="btn_g ">Continue with google</button>
             </div>
         </>
     );
